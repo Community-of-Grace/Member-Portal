dda1e371fb45f8567880fdaf8a20458d1c81b1c8
Brayden Price <brayden@braydenprice.dev>
2025-02-22 15:12:08 -0500

    Change deploy to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import clerk from "@clerk/astro";
-import cloudflare from '@astrojs/cloudflare'
+// import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
+// import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [clerk()],
-    adapter: cloudflare(),
-    output: "server",
+    adapter: vercel()
 });
 
 
