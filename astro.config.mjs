133523203e06349b2bdccf4cb8778bcfc36ddeb1
Brayden Price <brayden@braydenprice.dev>
2025-02-22 13:39:46 -0500

    Add Clerk Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import clerk from "@clerk/astro";
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [clerk()],
+    adapter: cloudflare(),
+    output: "server",
+});
+
+
+
+// Clerk Config
