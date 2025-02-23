adf4f786c50653d649a3a396f27a349b068323cd
Brayden Price <brayden@braydenprice.dev>
2025-02-22 19:59:41 -0500

    Update Nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import { defineConfig } from 'astro/config';
 import clerk from "@clerk/astro";
 // import node from "@astrojs/node";
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 // import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [clerk()],
-    adapter: vercel()
+  integrations: [clerk(), icon()],
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
 
 
